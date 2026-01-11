ee44aadaf7a90cc5bc3337fa19b9d603824557ab
Stephen Malone <you@yourdomain.com>
2026-01-11 17:35:57 +0100

    auto-claude: subtask-5-1 - Import EvolutionModule in ghl.module.ts

--- src/ghl/ghl.module.ts
+++ src/ghl/ghl.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { GhlService } from "./ghl.service";
 import { GhlTransformer } from "./ghl.transformer";
 import { GhlController } from './ghl.controller';
+import { EvolutionModule } from "../evolution/evolution.module";
 
 @Module({
+	imports: [EvolutionModule],
 	providers: [GhlService, GhlTransformer],
 	exports: [GhlService, GhlTransformer],
 	controllers: [GhlController],
